f3e4fcb6f1e9afc2e91fce9531cac1179b24fae4
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2020-06-13 22:53:58 +0600

    Document Model Updated

--- database/migrations/2020_06_13_162622_create_documents_table.php
+++ database/migrations/2020_06_13_162622_create_documents_table.php
@@ -40,6 +40,24 @@ class CreateDocumentsTable extends Migration
                 ->nullable()
                 ->comment('Size in KiloByte');
 
+            $table->softDeletes('deleted_at', 0);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('admins')
+                ->onDelete('cascade');
+            $table->foreign('updated_by')
+                ->references('id')
+                ->on('admins')
+                ->onDelete('cascade');
+            $table->foreign('deleted_by')
+                ->references('id')
+                ->on('admins')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
