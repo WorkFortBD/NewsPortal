890716f9dfab1f5b71e47bc5a024fb4f896f89ec
Md. Nazmus Shakib <shakib.corp@akij.net>
2020-06-27 11:40:03 +0600

    Updated Slider Model

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'image', 'is_button_enable', 'button_text', 'button_link', 'is_blank_redirect', 'is_description_enable', 'short_description', 'status', 'deleted_at', 'created_by', 'updated_by', 'deleted_by'
+    ];
 }
