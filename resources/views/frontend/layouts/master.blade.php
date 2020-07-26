df6618a628ab73f159b35848ba0cb048fdb00077
Md. Nazmus Shakib <shakib.corp@akij.net>
2020-07-26 12:50:37 +0600

    Added google analytics

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>@yield('title', config('app.name'))</title>
+    @include('frontend.layouts.partials.analytical')
     @include('frontend.layouts.partials.meta_tags')
     @yield('social_meta_tags')
     @include('frontend.layouts.partials.styles')
