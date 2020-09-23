5b64dcab2ef3fc3130152d19e566324a77d0359f
Md. Nazmus Shakib <shakib.corp@akij.net>
2020-09-24 01:37:49 +0600

    Changed design of entertainment section

--- public/assets/frontend/js/custom.js
+++ public/assets/frontend/js/custom.js
@@ -1,37 +1,32 @@
-
 (function ($) {
-    'use strict'
+  "use strict";
 
-    
-    
-        $('.enternaimentNews').owlCarousel({
-            loop: true,
-            dots: true,
-            dotsEach: true,
-            autoplay: false,
-            autoplayTimeout: 9000,
-            smartSpeed: 1000,
-            nav: true,
-            margin:20,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false,
-                },
-                400: {
-                    items: 2,
-                    nav: false,
-                },
-                991: {
-                    items: 4
-                },
-                1000: {
-                    items: 5
-                }
-            }
-        });
-     
-     
-
+  $(".enternaimentNews").owlCarousel({
+    loop: true,
+    dots: true,
+    dotsEach: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    nav: true,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 2,
+        nav: false,
+      },
+      991: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 })(jQuery);
