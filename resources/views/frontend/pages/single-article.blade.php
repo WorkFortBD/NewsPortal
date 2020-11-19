07ff4b2a7a263bc82b05dd1be337cca5f77ecc94
Md. Nazmus Shakib <shakib.corp@akij.net>
2020-11-19 15:57:33 +0600

    Changed date time format

--- resources/views/frontend/pages/single-article.blade.php
+++ resources/views/frontend/pages/single-article.blade.php
@@ -47,12 +47,26 @@
                         <a href="#">নিজস্ব প্রতিবেদক, যশোর</a>
                         {{-- <span>২৮ মে ২০২০, ০১:২৭</span> --}}
                         @php
-                            $time1 = App\Helpers\Dates\DateHelper::getBanglaDate();
-                            $time2 = App\Helpers\Dates\DateHelper::getArabicDate();
+                            // $time1 = App\Helpers\Dates\DateHelper::getBanglaDate();
+                            // $time2 = App\Helpers\Dates\DateHelper::getArabicDate();
+
+                            $currentDate = date("l, F j, Y");
+    
+                            $engDATE = array('1','2','3','4','5','6','7','8','9','0','January','February','March','April',
+                            'May','June','July','August','September','October','November','December','Saturday','Sunday',
+                            'Monday','Tuesday','Wednesday','Thursday','Friday');
+                            
+                            $bangDATE = array('১','২','৩','৪','৫','৬','৭','৮','৯','০','জানুয়ারী','ফেব্রুয়ারী','মার্চ','এপ্রিল','মে',
+                            'জুন','জুলাই','আগস্ট','সেপ্টেম্বর','অক্টোবর','নভেম্বর','ডিসেম্বর','শনিবার','রবিবার','সোমবার','মঙ্গলবার','
+                            বুধবার','বৃহস্পতিবার','শুক্রবার' 
+                            );
+                            
+                            $convertedDATE = str_replace($engDATE, $bangDATE, $currentDate);
                         @endphp
                         <span>
-                            {{ $time1 }},
-                            {{ $time2 }}
+                            {{ $convertedDATE }}
+                            {{--  {{ $time1 }},  --}}
+                            {{--  {{ $time2 }}  --}}
                         </span>
                     </div>
                     <div class="single-socle">
